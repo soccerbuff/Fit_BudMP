1c0cb990c9d95736b637fafaedf2fde2e6751a27
soccerbuff <89678292+soccerbuff@users.noreply.github.com>
2023-02-03 22:22:29 +0530

    Update links.py
    Signed-off-by: soccerbuff <89678292+soccerbuff@users.noreply.github.com>

--- links.py
+++ links.py
@@ -1,14 +1,38 @@
 import random
+
+#Beginner
 cardio_u45="https://www.youtube.com/watch?v=CS7uNgsrlYA"
 cardio_g=["https://www.youtube.com/watch?v=bO6NNfX_1ns","https://www.youtube.com/watch?v=5TWeosCGnC0"]
 cardio_g45=random.choice(cardio_g)
 respu45="https://www.youtube.com/watch?v=hJNDrSwf9dA"
 resp="https://www.youtube.com/watch?v=Kp8WK4hFsTs"
 aero_w="https://www.youtube.com/watch?v=WTUruNwUMFI"
+obese_men="https://www.youtube.com/watch?v=ksS1ypnjXw0"
+bob_brad="https://www.youtube.com/watch?v=HJByH8S9Tfw"
+collar_bone="https://www.youtube.com/watch?v=TAe_Sh7Uy8M"
+met="https://www.youtube.com/watch?v=STBGK7cgEDo"
+wrist="https://www.youtube.com/watch?v=48CshMyhK_g"
+spine="https://www.youtube.com/watch?v=2VuLBYrgG94"
+disc_bulge="https://www.youtube.com/watch?v=v-99BKSwP2A"
+arth="https://www.youtube.com/watch?v=VWMxjFuYHPc"
+#no probs
+beg_upp="https://www.youtube.com/watch?v=XNgP5HsjcbE"
+beg_leg="https://www.youtube.com/watch?v=xzIbXYX3Fes"
+beg_core="https://www.youtube.com/watch?v=6Q-crQuxME0"
+beg_end="https://www.youtube.com/watch?v=dj03_VDetdw"
+yog_beg="https://www.youtube.com/watch?v=c8hjhRqIwHE"
 
-
-
-
+#intermediate
+int_upp="https://www.youtube.com/watch?v=ZkvmYp5sFzw"
+int_low="https://www.youtube.com/watch?v=RoAaV-iMcBk"
+int_core="https://www.youtube.com/watch?v=GBn9zakStno"
+int_uppb="https://www.youtube.com/watch?v=b3Uxy3uwCgE"
+int_lowb="https://www.youtube.com/watch?v=R34Qjj3b-1U"
+int_coreb="https://www.youtube.com/watch?v=dJlFmxiL11s"
+int_end="https://www.youtube.com/watch?v=ELZSzEXNlaU"
+str_yog="https://www.youtube.com/watch?v=52GAcwujm0k"
+pow_yoga="https://www.youtube.com/watch?v=ZbtVVYBLCug"
+flex_yoga="https://www.youtube.com/watch?v=HSSP4dvmty4"
 #advanced
 foot_plyo=["https://www.youtube.com/watch?v=DOymvQX3voI","https://www.youtube.com/watch?v=3rpInu4XYM4"]
 plyo_f=random.choice(foot_plyo)
@@ -16,7 +40,7 @@ plyo_f=random.choice(foot_plyo)
 cricket_plyo=["https://www.youtube.com/watch?v=0R-zZ3AK0Vw","https://www.youtube.com/watch?v=R9lEYJs5N2I"]
 plyo_c=random.choice(cricket_plyo)
 
-basket_plyo=["https://www.youtube.com/watch?v=HRGU3igMytM","https://www.youtube.com/watch?v=x84r0G2gYII"]
+basket_plyo=["https://www.youtube.com/watch?v=9qX6OlAVA9s","https://www.youtube.com/watch?v=HRGU3igMytM","https://www.youtube.com/watch?v=x84r0G2gYII"]
 plyo_b=random.choice(basket_plyo)
 
 #adv-non-ath
@@ -26,3 +50,8 @@ adv_upper_w=["https://www.youtube.com/watch?v=M1ZfcylH9-0","https://www.youtube.
 wm_upper=random.choice(adv_upper_w)
 adv_lower_m="https://www.youtube.com/watch?v=erKJOb51bCI"
 adv_lower_w="https://www.youtube.com/watch?v=UEWEYeJGkLM"
+adv_core_w="https://www.youtube.com/watch?v=AnYl6Nk9GOA"
+adv_core_m="https://www.youtube.com/watch?v=-hqYWRib5y8"
+end_m="https://www.youtube.com/watch?v=vrhYYxnulZY"
+end_wdum="https://www.youtube.com/watch?v=zI3wf2iviJc"
+end_wndum="https://www.youtube.com/watch?v=kZDvg92tTMc"
